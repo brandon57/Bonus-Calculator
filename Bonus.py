574af5715ea83d0f863dc6220f388e734b304c69
brandon57 <bmsitarz@gmail.com>
2024-03-23 23:19:20 -0500

    Added it where if people have a score of 2 or less they get put into a no_bonus list

--- Bonus.py
+++ Bonus.py
@@ -9,6 +9,7 @@ class Person:
 if __name__ == "__main__":
     total_money = 5000
     people = []
+    no_bonus = []
     row_num = 0
     file = ""
     while True:
@@ -24,8 +25,16 @@ if __name__ == "__main__":
         csv_file = csv.reader(csvfile, delimiter=',')
         for row in csv_file:
             if row_num != 0:
-                people.append(Person(row[0], int(row[1])))
+                if(int(row[1]) > 2):
+                    people.append(Person(row[0], int(row[1])))
+                else:
+                    no_bonus.append(Person(row[0], int(row[1])))
             row_num += 1
     
-    for x in range(len(people)):
-        print(people[x].name + ", " + str(people[x].score))
+    print("These are the people who will get a bonus:")
+    for x in people:
+        print(x.name)
+    
+    print("\nThese are the people who will not get a bonus:")
+    for x in no_bonus:
+        print(x.name)
